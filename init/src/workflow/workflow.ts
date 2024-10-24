dd1ee59459b57723026c5125bce065f7ba119751
Vladislav Polyakov <polrk@yandex-team.ru>
2024-10-24 15:05:57 +0300

    🔥 Mergins head and abse metrics

--- init/src/workflow/workflow.ts
+++ init/src/workflow/workflow.ts
@@ -2,7 +2,7 @@ import { getInput } from "@actions/core"
 import { context, getOctokit } from "@actions/github"
 
 export async function getCurrentWorkflowRuns(branch: string) {
-	let { data: { workflows } } = await getOctokit(getInput("token", { required: true })).rest.actions.listRepoWorkflows({
+	let { data: { workflows } } = await getOctokit(getInput("github_token", { required: true })).rest.actions.listRepoWorkflows({
 		owner: context.repo.owner,
 		repo: context.repo.repo,
 	})
@@ -13,7 +13,7 @@ export async function getCurrentWorkflowRuns(branch: string) {
 		return []
 	}
 
-	let { data: { workflow_runs } } = await getOctokit(getInput("token", { required: true })).rest.actions.listWorkflowRuns({
+	let { data: { workflow_runs } } = await getOctokit(getInput("github_token", { required: true })).rest.actions.listWorkflowRuns({
 		owner: context.repo.owner,
 		repo: context.repo.repo,
 		workflow_id: workflow.id,
