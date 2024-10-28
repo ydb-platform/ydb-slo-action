1171eeaba3b6dde55cb8a700e85f4ad0d3341bc8
Vladislav Polyakov <polRk@ydb.tech>
2024-10-29 00:15:50 +0300

    🧪 Test matrix jobs

--- report/src/jobs/main.ts
+++ report/src/jobs/main.ts
@@ -1,36 +1,19 @@
 import { DefaultArtifactClient } from '@actions/artifact';
-import { debug, getInput } from '@actions/core'
+import { debug, getInput, info } from '@actions/core'
 import { context } from '@actions/github';
 
 (async function main() {
 	debug(JSON.stringify(context, null, 4))
-	let artifact = new DefaultArtifactClient();
+	let artifactClient = new DefaultArtifactClient();
 
-	{
+	let { artifacts } = await artifactClient.listArtifacts({
+		findBy: {
+			token: getInput("token", { required: true }),
+			workflowRunId: parseInt(getInput("run_id", { required: true })),
+			repositoryOwner: context.repo.owner,
+			repositoryName: context.repo.repo,
+		}
+	});
 
-		let response = await artifact.getArtifact(`report.md`, {
-			findBy: {
-				token: getInput("token", { required: true }),
-				workflowRunId: parseInt(getInput("run_id", { required: true })),
-				repositoryOwner: context.repo.owner,
-				repositoryName: context.repo.repo,
-			}
-		});
-
-		debug(JSON.stringify(response.artifact, null, 4))
-	}
-
-	{
-
-		let response = await artifact.getArtifact(`pull.txt`, {
-			findBy: {
-				token: getInput("token", { required: true }),
-				workflowRunId: parseInt(getInput("run_id", { required: true })),
-				repositoryOwner: context.repo.owner,
-				repositoryName: context.repo.repo,
-			}
-		});
-
-		debug(JSON.stringify(response.artifact, null, 4))
-	}
+	info(`Found ${artifacts.length} artifacts: ${JSON.stringify(artifacts, null, 4)}`)
 })()
