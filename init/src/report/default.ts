810079f26fed8e0e54209837930f3ce6df21b4cd
Vladislav Polyakov <polRk@ydb.tech>
2024-10-29 00:15:52 +0300

    🧪 Storing metrics for future comparisons

--- init/src/report/default.ts
+++ init/src/report/default.ts
@@ -1,27 +1,22 @@
 import { renderChart, type Series } from "./chart";
 
 export const renderReport = (sdk: string, metrics: Record<string, Series[]>) => `# SLO Testing <img align="right" height="36" src="https://img.shields.io/badge/⎇-head-%23FF7F0E?style=for-the-badge"><img align="right" height="36"  src="https://img.shields.io/badge/⎇-base-%231F77B4?style=for-the-badge"><img align="right" height="36" src="https://img.shields.io/badge/sdk-${sdk.replace(/-/g, "--")}-%23E377C2?style=for-the-badge">
-<details><summary>Operation Success Rate</summary>
 
-${renderChart("operation_type=read", metrics["read_availability"])}
+### Operation Success Rate
 
-${renderChart("operation_type=write", metrics["write_availability"])}
+${renderChart("operation_type=read", metrics["read_availability"], "Time, m", "Success Rate, %")}
 
-</details>
+${renderChart("operation_type=write", metrics["write_availability"], "	Time, m", "Success Rate, %")}
 
-<details><summary>Operations Per Second</summary>
+### Operations Per Second
 
-${renderChart("operation_type=read", metrics["read_throughput"])}
+${renderChart("operation_type=read", metrics["read_throughput"], "Time, m", "Operations")}
 
-${renderChart("operation_type=write", metrics["write_throughput"])}
+${renderChart("operation_type=write", metrics["write_throughput"], "Time, m", "Operations")}
 
-</details>
+### 95th Percentile Latency
 
-<details><summary>95th Percentile Latency</summary>
+${renderChart("operation_type=read", metrics["read_latency_ms"], "Time, m", "Latency, ms")}
 
-${renderChart("operation_type=read", metrics["read_latency_ms"])}
-
-${renderChart("operation_type=write", metrics["write_latency_ms"])}
-
-</details>
+${renderChart("operation_type=write", metrics["write_latency_ms"], "Time, m", "Latency, ms")}
 `
