7321545b7ceb9890567d4adcebd77a85a877c2b6
Vladislav Polyakov <polRk@ydb.tech>
2024-10-29 00:15:49 +0300

    🧪 Store pull reqeust number

---
+++ init/src/help/pulls.ts
@@ -0,0 +1,33 @@
+import { getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+export async function getPullRequestNumber() {
+	let token = getInput('github_token') || process.env.GITHUB_TOKEN;
+
+	let prNumber = getInput("github_pull_request_number");
+	if (prNumber.length > 0) {
+		return parseInt(prNumber);
+	}
+
+	if (context.eventName === 'pull_request') {
+		return context.payload.pull_request!.number;
+	}
+
+	if (token) {
+		let octokit = getOctokit(token);
+		let branch = context.ref.replace('refs/heads/', '');
+
+		const { data: pulls } = await octokit.rest.pulls.list({
+			state: 'open',
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			head: `${context.actor}:${branch}`,
+		});
+
+		if (pulls.length > 0) {
+			return pulls[0].number;
+		}
+	}
+
+	return null
+}
--- report/src/jobs/main.ts
+++ report/src/jobs/main.ts
@@ -3,16 +3,34 @@ import { debug, getInput } from '@actions/core'
 import { context } from '@actions/github';
 
 (async function main() {
+	debug(JSON.stringify(context, null, 4))
 	let artifact = new DefaultArtifactClient();
 
-	let response = await artifact.getArtifact(`report.md`, {
-		findBy: {
-			token: getInput("token", { required: true }),
-			workflowRunId: parseInt(getInput("run_id", { required: true })),
-			repositoryOwner: context.repo.owner,
-			repositoryName: context.repo.repo,
-		}
-	});
+	{
 
-	debug(JSON.stringify(response.artifact, null, 4))
+		let response = await artifact.getArtifact(`report.md`, {
+			findBy: {
+				token: getInput("token", { required: true }),
+				workflowRunId: parseInt(getInput("run_id", { required: true })),
+				repositoryOwner: context.repo.owner,
+				repositoryName: context.repo.repo,
+			}
+		});
+
+		debug(JSON.stringify(response.artifact, null, 4))
+	}
+
+	{
+
+		let response = await artifact.getArtifact(`pull.txt`, {
+			findBy: {
+				token: getInput("token", { required: true }),
+				workflowRunId: parseInt(getInput("run_id", { required: true })),
+				repositoryOwner: context.repo.owner,
+				repositoryName: context.repo.repo,
+			}
+		});
+
+		debug(JSON.stringify(response.artifact, null, 4))
+	}
 })()
