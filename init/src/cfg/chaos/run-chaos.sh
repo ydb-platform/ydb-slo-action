76066550facebcb874fcb19a256add35be7dac4e
Vladislav Polyakov <polRk@ydb.tech>
2025-07-15 14:44:04 +0300

    🧪 Update healthcheck conditions for database services and refactor chaos script for clarity
    Signed-off-by: Vladislav Polyakov <polRk@ydb.tech>

--- init/src/cfg/chaos/run-chaos.sh
+++ init/src/cfg/chaos/run-chaos.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 
-get_random_container() {
-    # Get a list of all containers starting with ydb-database-*
-    containers=$(docker ps --format '{{.Names}}' | grep '^ydb-database-')
+get_random_ydb_database_container() {
+    # Get a list of all containers starting with database-*
+    local containers=$(docker ps --format '{{.Names}}' | grep '^database-')
 
     # Convert the list to a newline-separated string
-    containers=$(echo "$containers" | tr ' ' '\n')
+    local containers=$(echo "$containers" | tr ' ' '\n')
 
     # Count the number of containers
-    containersCount=$(echo "$containers" | wc -l)
+    local containersCount=$(echo "$containers" | wc -l)
 
     # Generate a random number between 0 and containersCount - 1
-    randomIndex=$(shuf -i 0-$(($containersCount - 1)) -n 1)
+    local randomIndex=$(shuf -i 0-$(($containersCount - 1)) -n 1)
 
     # Get the container name at the random index
     nodeForChaos=$(echo "$containers" | sed -n "$(($randomIndex + 1))p")
@@ -19,18 +19,18 @@ get_random_container() {
 
 sleep 60
 
-get_random_container
+get_random_ydb_database_container
 sh -c "docker stop ${nodeForChaos} -t 30"
 sh -c "docker start ${nodeForChaos}"
 
 sleep 60
 
-get_random_container
+get_random_ydb_database_container
 sh -c "docker restart ${nodeForChaos} -t 0"
 
 sleep 60
 
-get_random_container
+get_random_ydb_database_container
 sh -c "docker kill -s SIGKILL ${nodeForChaos}"
 
 sleep 60
