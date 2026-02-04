0fd87fba9095b04689853e83395139a17faf3014
Vladislav Polyakov <polRk@ydb.tech>
2026-02-04 16:21:25 +0300

    🔧 Build actions

--- dist/report/main.js
+++ dist/report/main.js
@@ -2,14 +2,14 @@ import {
   compareWorkloadMetrics,
   formatChange,
   formatValue
-} from "../main-na54qh2e.js";
+} from "../main-d4a954td.js";
 import {
   __toESM,
   require_artifact,
   require_core,
   require_exec,
   require_github
-} from "../main-mj2ce5f3.js";
+} from "../main-1qcptng8.js";
 
 // report/main.ts
 var import_artifact2 = __toESM(require_artifact(), 1), import_core4 = __toESM(require_core(), 1), import_github3 = __toESM(require_github(), 1);
